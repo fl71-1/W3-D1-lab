2e54b007c4de6ca9b6fb06cff627ea4e0ad93691
fahd alghtni <fahdalghtni246@gmail.com>
2024-09-08 12:35:57 +0300

    W3-D1-lab

---
+++ min.js
@@ -0,0 +1,40 @@
+
+
+
+let eqle ="The area of the rectangle is"
+let Height =5;
+let width =6;
+console.log( eqle, Height + width);
+
+let Celsius= 30;
+let Fahrenheit = 120;
+let Fahrenheiteql =(Celsius + 9/5)+32;
+let Celsiuseql =(Fahrenheit - 32)/9*5;
+console.log(Fahrenheiteql);
+console.log(Celsiuseql)
+
+let hours =1;
+let seconds =60 * 60 * hours ;
+console.log (seconds);
+
+let thingOne =" iam fahd "
+let thingTwo ="iam in tuwaiq"
+console.log(thingOne + thingTwo);
+
+let bill=10.25 + 3.99 + 7.15;
+let tip=0.15;
+let total = bill+tip;
+console.log(total);
+
+var adjective1 ="amazing";
+var adjective2 = "fun";
+var adjective3 = "entertaining";
+let madLib ="The Intro to JavaScript course is"+adjective1+  "James and Julia are so"+adjective2+ "I cannot wait to work through the rest of this"+adjective3+ "content!";
+console.log(madLib);
+
+let firstName = "fahd" ;
+let interest = "hobby";
+let hobby = "tea" ;
+let awesomeMessage ="Hi, my name is"+firstName+" I love"+interest+ "In my spare time, I like to"+hobby;
+console.log(awesomeMessage);
+
