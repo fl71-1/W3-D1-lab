b5ddda5bc8beac792c2f813fded15ac662cd9f8f
fahd alghtni <fahdalghtni246@gmail.com>
2024-09-08 14:56:18 +0300

    lab2

--- min.js
+++ min.js
@@ -1,40 +1,55 @@
 
+   
+   var eatsPlants = "false";
+   var eatsAnimals = "true";
+ 
+   let Food = eatsAnimals && eatsPlants ?"omnivore" 
+   :eatsPlants? "herbivore" 
+   :eatsAnimals?"carnivore"
+   :"undefined";
+   console.log(food);
+   
+
+   var musicians = 0;
+  if (musicians<=0){
+    console.log("not a group");
+
+  }else if (musicians === 1){
+    console.log("solo");
+    
+  }else if (musicians === 2){
+    console.log("duet");
+  }else if (musicians === 3){
+    console.log("trio");
+  }else if(musicians === 4){
+    console.log("quartet");
+    
+  }else if(musicians>4){
+    console.log("this is a large group");
+  }
+  
+ let balance = 1;
+ let checkBalance ="true";
+ let isActive ="true";
+ if (checkBalance===true)
 
 
-let eqle ="The area of the rectangle is"
-let Height =5;
-let width =6;
-console.log( eqle, Height + width);
+    if(isActive===true){
+        console.log(balance);
+    
 
-let Celsius= 30;
-let Fahrenheit = 120;
-let Fahrenheiteql =(Celsius + 9/5)+32;
-let Celsiuseql =(Fahrenheit - 32)/9*5;
-console.log(Fahrenheiteql);
-console.log(Celsiuseql)
 
-let hours =1;
-let seconds =60 * 60 * hours ;
-console.log (seconds);
+ }
 
-let thingOne =" iam fahd "
-let thingTwo ="iam in tuwaiq"
-console.log(thingOne + thingTwo);
+ 
+  
+  let flavor ="vanilla";
+  let vessel ="cone";
+  let toppings="sprinkles";
+ if(
+    (flavor === "vailla"||"chocolate")&&(vessel=== "cone"||"bowl")&&(toppings==="sprinkles"||"peanuts")
 
-let bill=10.25 + 3.99 + 7.15;
-let tip=0.15;
-let total = bill+tip;
-console.log(total);
-
-var adjective1 ="amazing";
-var adjective2 = "fun";
-var adjective3 = "entertaining";
-let madLib ="The Intro to JavaScript course is"+adjective1+  "James and Julia are so"+adjective2+ "I cannot wait to work through the rest of this"+adjective3+ "content!";
-console.log(madLib);
-
-let firstName = "fahd" ;
-let interest = "hobby";
-let hobby = "tea" ;
-let awesomeMessage ="Hi, my name is"+firstName+" I love"+interest+ "In my spare time, I like to"+hobby;
-console.log(awesomeMessage);
-
+ ){
+    console.log("I'd like two scoops of"+flavor+"ice cream in a"+vessel+"with"+toppings+".")
+ }
+    
